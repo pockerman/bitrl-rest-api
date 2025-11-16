99127a370103995fe063ea0e9314562f8b0329d4
Alex Giavaras <alex@trilodocs.com>
2025-11-16 09:19:09 +0000

    Update cart pole api

--- api/gymnasium_envs/classic_control/cart_pole_env_api.py
+++ api/gymnasium_envs/classic_control/cart_pole_env_api.py
@@ -1,19 +1,20 @@
 import gymnasium as gym
 from typing import Any
 from loguru import logger
+import sys
 from fastapi import APIRouter, Body, status
 from fastapi.responses import JSONResponse
 from fastapi import HTTPException
 from api.utils.time_step_response import TimeStep, TimeStepType
+from api.utils.gym_env_manager import GymEnvManager
 
-cart_pole_router = APIRouter(prefix="/gymnasium/cart-pole-env", tags=["cart-pole-env"])
+cart_pole_router = APIRouter(prefix="/gymnasium/cart-pole-env",
+                             tags=["cart-pole-env"])
 
 ENV_NAME = "CartPole"
 
-# the environments to create
-envs = {
-    0: None
-}
+# the manager for the environments to create
+manager = GymEnvManager(verbose=True)
 
 # actions that the environment accepts
 ACTIONS_SPACE = {0: "Push cart to the left", 1: "Push cart to the right"}
@@ -25,145 +26,110 @@ async def get_action_space() -> JSONResponse:
                         content={"action_space": ACTIONS_SPACE})
 
 
-@cart_pole_router.get("/is-alive")
-async def get_is_alive(cidx: int) -> JSONResponse:
-    global envs
-    if cidx in envs:
-        env = envs[cidx]
+@cart_pole_router.get("/{idx}/is-alive")
+async def get_is_alive(idx: int) -> JSONResponse:
+    is_alive_ = manager.is_alive(idx=idx)
 
-        if env is None:
-            return JSONResponse(status_code=status.HTTP_200_OK,
-                                content={"result": False})
-        else:
-            return JSONResponse(status_code=status.HTTP_200_OK,
-                                content={"result": True})
-    else:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
-                            content={"message": f"Environment {ENV_NAME} and index {cidx} has not been created"})
+    return JSONResponse(status_code=status.HTTP_200_OK,
+                        content={"result": is_alive_})
 
 
-@cart_pole_router.post("/close")
-async def close(cidx: int) -> JSONResponse:
-    global envs
-    if cidx in envs:
-        env = envs[cidx]
-        if env is not None:
-            envs[cidx].close()
-            envs[cidx] = None
-            logger.info(f'Closed environment {ENV_NAME}  and index {cidx}')
-            return JSONResponse(status_code=status.HTTP_202_ACCEPTED,
-                                content={"message": f"Environment {ENV_NAME} and index {cidx} is closed"})
+@cart_pole_router.post("/{idx}/close")
+async def close(idx: int) -> JSONResponse:
+    closed = await manager.close(idx=idx)
+
+    if closed:
+        return JSONResponse(status_code=status.HTTP_202_ACCEPTED,
+                            content={"message": "OK"})
 
     return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
-                        content={"message": f"Environment {ENV_NAME} and index {cidx} has not been created"})
+                        content={"message": "FAILED"})
 
 
-@cart_pole_router.post("/make")
-async def make(version: str = Body(default="v1"),
-               cidx: int = Body(...),
+@cart_pole_router.post("/{idx}/make")
+async def make(idx: int,
+               version: str = Body(default="v1"),
                options: dict[str, Any] = Body(default={})) -> JSONResponse:
-    global envs
+
     env_type = f"{ENV_NAME}-{version}"
-    if cidx in envs:
-        env = envs[cidx]
+    await manager.make(idx=idx, env_name=env_type,
+                       **options)
 
-        if env is not None:
-            envs[cidx].close()
-
-        try:
-
-            if len(options) != 0:
-                env = gym.make(env_type, **options)
-            else:
-                env = gym.make(env_type,)
-            envs[cidx] = env
-        except Exception as e:
-            logger.error('An exception was raised')
-            logger.opt(exception=e).info("Logging exception traceback")
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail=str(e))
-    else:
-        try:
-            env = gym.make(env_type)
-            envs[cidx] = env
-        except Exception as e:
-            logger.error('An exception was raised')
-            logger.opt(exception=e).info("Logging exception traceback")
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                detail=str(e))
-
-    logger.info(f'Created environment {ENV_NAME} and index {cidx}')
+    logger.info(f'Created environment  {ENV_NAME} and index {idx}')
     return JSONResponse(status_code=status.HTTP_201_CREATED,
                         content={"result": True})
 
 
-@cart_pole_router.post("/reset")
-async def reset(seed: int = Body(default=42),
-                cidx: int = Body(...),
+
+
+@cart_pole_router.post("/{idx}/reset")
+async def reset(idx: int,
+                seed: int = Body(default=42),
                 options: dict[str, Any] = Body(default={})) -> JSONResponse:
     """Reset the environment
 
     :return:
     """
+    if idx not in manager:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail={"message": "NOT_ALIVE/NOT_CREATED"})
 
-    global envs
-    if cidx in envs:
-        env = envs[cidx]
-
-        if env is not None:
-            observation, info = envs[cidx].reset(seed=seed)
-            observation = [float(val) for val in observation]
-            step = TimeStep(observation=observation,
-                            reward=0.0,
-                            step_type=TimeStepType.FIRST,
-                            info=info,
-                            discount=1.0)
-            logger.info(f'Reset environment {ENV_NAME}  and index {cidx}')
-            return JSONResponse(status_code=status.HTTP_202_ACCEPTED,
-                                content={"time_step": step.model_dump()})
-
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                        detail={"message": f"Environment {ENV_NAME} is not initialized."
-                                           " Have you called make()?"})
+    try:
+        reset_step = await manager.reset(idx=idx, seed=seed)
 
 
-@cart_pole_router.post("/step")
-async def step(action: int = Body(...), cidx: int = Body(...)) -> JSONResponse:
+        observation = reset_step.observation
+        observation = [float(val) for val in observation]
+        step_ = TimeStep(observation=observation,
+                         reward=0.0,
+                         step_type=TimeStepType.FIRST,
+                         info=reset_step.info,
+                         discount=1.0)
+        return JSONResponse(status_code=status.HTTP_202_ACCEPTED,
+                            content={"time_step": step_.model_dump()})
+    except Exception as e:
+        exception = sys.exc_info()
+        logger.opt(exception=exception).info("Logging exception traceback")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail={"message": f"Environment {ENV_NAME} is not initialized."
+                                               " Have you called make()?"})
+
+
+
+@cart_pole_router.post("/{idx}/step")
+async def step(idx: int, action: int = Body(...)) -> JSONResponse:
+
+    if idx not in manager:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail={"message": "NOT_ALIVE/NOT_CREATED. Call make/reset"})
 
     if action not in ACTIONS_SPACE:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"Action {action} not in {list(ACTIONS_SPACE.keys())}")
 
-    global envs
-    if cidx in envs:
-        env = envs[cidx]
+    step_result = await manager.step(idx=idx, action=action)
 
-        if env is not None:
-            observation, reward, terminated, truncated, info = envs[cidx].step(action)
-            observation = [float(val) for val in observation]
+    step_type = TimeStepType.MID
+    if step_result.terminated:
+        step_type = TimeStepType.LAST
 
-            step_type = TimeStepType.MID
-            if terminated:
-                step_type = TimeStepType.LAST
-
-            if info is not None:
-                info['truncated'] = truncated
-
-            step = TimeStep(observation=observation,
-                            reward=reward,
-                            step_type=step_type,
-                            info=info,
-                            discount=1.0)
-
-            logger.info(f'Step in environment {ENV_NAME} and index {cidx}')
-            return JSONResponse(status_code=status.HTTP_202_ACCEPTED,
-                                content={"time_step": step.model_dump()})
-
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                        detail=f"Environment {ENV_NAME} is not initialized. Have you called make()?")
+    info = step_result.info
+    if info is not None:
+        info['truncated'] = step_result.truncated
 
 
-@cart_pole_router.post("/sync")
-async def sync(cidx: int = Body(...), options: dict[str, Any] = Body(default={})) -> JSONResponse:
+    observation = [float(val) for val in step_result.observation]
+    step_ = TimeStep(observation=observation,
+                    reward=step_result.reward,
+                    step_type=step_type,
+                    info=info,
+                    discount=1.0)
+
+    logger.info(f'Step in environment {ENV_NAME} and index {idx}')
     return JSONResponse(status_code=status.HTTP_202_ACCEPTED,
-                        content={"message": "OK"})
+                        content={"time_step": step_.model_dump()})
+
+# @cart_pole_router.post("/sync")
+# async def sync(cidx: int = Body(...), options: dict[str, Any] = Body(default={})) -> JSONResponse:
+#     return JSONResponse(status_code=status.HTTP_202_ACCEPTED,
+#                         content={"message": "OK"})
